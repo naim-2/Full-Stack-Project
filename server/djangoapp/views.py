3dcc8249bfd93314070cc63963762a81a348762f
naim-2 <abdulhakimnaim@gmail.com>
2024-06-29 07:40:48 +0300

    Fix Flake-8 Linting Errors part 3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -112,7 +112,7 @@ def get_cars(request):
 # ...
 
 
-# Update the `get_dealerships` render list of dealerships all by default, 
+# Update the `get_dealerships` render list of dealerships all by default,
 # particular state if state is passed
 def get_dealerships(request, state="All"):
     if (state == "All"):
